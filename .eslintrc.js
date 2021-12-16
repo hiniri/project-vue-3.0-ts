98a223c436258d75f0e7ce08ea38bf562ff17f90
hefeng <hefeng_0705@163.com>
2021-12-16 16:06:28 +0800

    Eslint配置修改，添加Prettier格式化配置文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,24 +3,21 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
+  extends: ['plugin:vue/vue3-essential', '@vue/standard', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: [1, 'single'],
+    semi: [2, 'never'],
+    'space-before-function-paren': [0, 'always'],
+    'comma-dangle': [2, 'never']
   },
   overrides: [
     {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
       env: {
         mocha: true
       }
