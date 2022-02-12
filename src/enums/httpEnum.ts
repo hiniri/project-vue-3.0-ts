eef6836f7008d3397e59ca7c362795dd3733c305
hefeng <hefeng_0705@163.com>
2022-02-12 09:44:49 +0800

    新增 http 状态枚举类型

---
+++ src/enums/httpEnum.ts
@@ -0,0 +1,31 @@
+/**
+ * 请求类型
+ *
+ * @export
+ * @enum {number}
+ */
+export enum MethodEnum {
+  GET = 'GET',
+  POST = 'POST',
+  PATCH = 'PATCH',
+  PUT = 'PUT',
+  DELETE = 'DELETE'
+}
+
+/**
+ * 传输格式
+ *
+ * @export
+ * @enum {number}
+ * @description
+ */
+export enum ContentTypeEnum {
+  // json
+  JSON = 'application/json;charset=UTF-8',
+  // json
+  TEXT = 'text/plain;charset=UTF-8',
+  // form-data 一般配合qs
+  FORM_URLENCODED = 'application/x-www-form-urlencoded;charset=UTF-8',
+  // form-data  上传
+  FORM_DATA = 'multipart/form-data;charset=UTF-8'
+}
